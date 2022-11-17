dd1a6eb5ac8ad151e954cea9a33fd084c91cfbe2
art <art@example.com>
2022-11-16 22:07:59 -0500

    fixed

---
+++ test.sh
@@ -0,0 +1,26 @@
+echo "en_US UTF UTF.9" > /etc/default/libc-locales
+
+[ -f "/etc/default/libc-locales" ] && echo "conf"
+[ -f "/etc/default/libc-locales" ] && echo "conf"
+[ -f "/etc/default/libc-locales" ] && echo "conf"
+
+sudo xbps-install feh
+
+
+
+
+sudo xbps-reconfigure -f glibc-locales
+sudo xbps-reconfigure -f glibc-locales
+sudo xbps-reconfigure -f glibc-locales
+sudo xbps-reconfigure -f glibc-locales
+
+echo "ar_AE ISO-8859-6" > /etc/default/libc-locales
+
+
+
+
+
+
+
+
+
